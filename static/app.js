00459b9701dcb4e242bfa4380263e97ef517d314
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-05-03 23:28:51 -0700

    added image choice functionality, cleaning code

--- static/app.js
+++ static/app.js
@@ -1,68 +1,131 @@
 "use strict";
 
-const API_ENDPOINT = '/image';
+const IMAGE_ENDPOINT = '/image';
+const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+
 const $submitBtn = $('#submit-btn');
 const $fileInput = $('#input-file');
 const $inputFileLabel = $('#input-file-label');
-const $image = $('img');
 const $imageWrapper = $('#image-wrapper');
 
-$submitBtn.on("click", updloadImage);
-$fileInput.on("change", showFileName);
 
+/**
+ * Handles changes in file input
+ * Displays the selected file name in the input label
+ * @param {*} evt
+ */
 function showFileName(evt) {
   if (evt.target.files.length) {
     $inputFileLabel.html(evt.target.files[0].name);
   }
 }
-
+$fileInput.on("change", showFileName);
 
 /**
- * Handles the creation of a new cupcake via cupcake form
- * Post request to /api/cupcakes
- * @param {*} evt
+ * Appends the image in 'file' to the DOM
+ * @param {*} file
  */
-async function updloadImage(evt) {
-  evt.preventDefault();
+function displayImage(file) {
+  var reader = new FileReader();
+
+  reader.onload = function () {
+    $imageWrapper.html("");
+
+    $imageWrapper.append(
+      $('<img>')
+        .css("height", "800px")
+        .css("width", "auto")
+        .css("position", "absolute")
+        .css("z-index", 2)
+        .attr('src', reader.result)
+    );
+  };
+
+  reader.readAsDataURL(file);
+}
+
+/**
+ * Sends request to flask with image in multipart/form-data
+ * Returns response with response.data structure
+ * {
+ *  boxes: [...,['C','1784','2187','1839','2254'],...]
+ *  dim: [int(img_height), int(img_width)]
+ *  msg:'success'
+ * }
+ * @param {file} file image file
+ */
+async function updloadImage(file) {
 
   let formData = new FormData();
+  formData.append("file", file);
 
-  if ($fileInput[0].files.length) {
-    formData.append("file", $fileInput[0].files[0]);
-  }
-  else {
-    return;
-  }
-
-
-
-  const response = await axios.post(API_ENDPOINT, formData, {
+  const response = await axios.post(IMAGE_ENDPOINT, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     }
   });
 
-  console.log(response.data.boxes);
-  overlay_boxes(response.data.boxes);
   return response;
-
 }
 
-function overlay_boxes(boxes) {
-  let height = parseInt($image.css("height"));
-  let width = parseInt($image.css("width"));
 
-  for (let box of boxes) {
+/**
+ * Accepts an object box_data:
+ * {
+ *  boxes: [...,['C','1784','2187','1839','2254'],...]
+ *  dim: [int(img_height), int(img_width)]
+ *  msg:'success'
+ * }
+ *  Adds corresponding divs to DOM inside imageWrapper div
+ * @param {object} box_data
+ */
+function displayBoxes(box_data) {
+  let height = parseInt($('img').css("height"));
+  let width = parseInt($('img').css("width"));
+  let ratio = height / box_data.dim[0];
+
+  for (let box of box_data.boxes) {
 
     $imageWrapper.append(
       $('<div>').html(
         box[0]
-      ).css("position", "relative")
-      .css("z-index", "3")
-      .css("display", "inline-block")
-      .css('height',`${(box[4]-box[2])*(height/4050)}px`)
-      .css('width',`${(box[3]-box[1])*(width/3240)}px`)
-      .css('border-style', 'solid')
+      ).css("position", "absolute")
+        .css("bottom", `${box[2] * ratio}px`)
+        .css("left", `${box[1] * ratio}px`)
+        .css("z-index", "3")
+        .css("display", "inline-block")
+        .css('height', `${(box[4] - box[2]) * (ratio)}px`)
+        .css('width', `${(box[3] - box[1]) * (ratio)}px`)
+        .css('border-style', 'solid')
     ).css('height', height).css('width', width);
   }
 }
+
+/**
+ * handles the event when user clicks submit button
+ * calls functions to:
+ *  display the uploaded image,
+ *  send image to flask server and recieve response
+ *  display boxes for corresponding letters in image
+ * @param {*} evt
+ */
+async function submitImage(evt) {
+  evt.preventDefault();
+
+  let file = $fileInput[0].files[0];
+  let fileType = file['type'];
+
+  //check if file uploaded is an image
+  if (validImageTypes.includes(fileType)) {
+
+    displayImage(file);
+    let response = await updloadImage(file);
+    console.log(response.data);
+    displayBoxes(response.data);
+
+  } else {
+    alert("bad file");
+  }
+}
+
+$submitBtn.on("click", submitImage);
